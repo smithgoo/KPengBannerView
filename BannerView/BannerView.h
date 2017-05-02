1823ee089adfeed0bcb5c793e5add146de47dfbf
王朋 <294336370@qq.com>
2017-05-02 12:06:10 +0800

    no message

---
+++ BannerView/BannerView.h
@@ -0,0 +1,13 @@
+//
+//  BannerView.h
+//  MainProject
+//
+//  Created by 王朋 on 2017/5/2.
+//  Copyright © 2017年 王朋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BannerView : UIView
+
+@end
